70ef662702c7d6ec51ef301662e74da77a18f3f4
Shubh Gupta <shubhgupta2510@gmail.com>
2025-09-24 12:23:13 +1000

    fix issues with agent not registering

--- mitm_entrypoint.sh
+++ mitm_entrypoint.sh
@@ -13,5 +13,5 @@ echo "Starting socat proxy..."
 # Terminate TLS from agent, re-send cleartext to registrar
 # -v -x cause socat to dump payloads in hex/ASCII in the logs
 exec socat -d -d -v \
-  OPENSSL-LISTEN:8891,fork,reuseaddr,cert=/etc/attacker_certs/mitm_server.crt,key=/etc/attacker_certs/mitm_server.key,verify=0 \
-  OPENSSL:keylime-registrar:8891,cert=/etc/attacker_certs/mitm_client.crt,key=/etc/attacker_certs/mitm_client.key,verify=0
+  TCP-LISTEN:8891,fork,reuseaddr \
+  TCP:keylime-registrar:8890
